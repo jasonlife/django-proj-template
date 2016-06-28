e8245afa290c9dbc8553a9703c15c79687f0c4f4
Jason Kim <jasonlife@gmail.com>
2016-06-28 06:52:37 -0600

    Added STATICFILES_DIR and MEDIA_ROOT

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -122,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [join(BASE_DIR, 'static')]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = join(BASE_DIR, 'media')
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 class MockUp(TemplateView):
@@ -28,3 +30,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^(?P<template_name>.*)$', MockUp.as_view()),
 ]
+
+# Serve user uploaded files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
